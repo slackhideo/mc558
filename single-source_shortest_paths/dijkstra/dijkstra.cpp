24c96c8b0eac242760f50cbf76b11157df0114d4
slackhideo <burajirugaijin@yahoo.co.jp>
2013-04-17 18:55:33 -0300

    Alterações no dijkstra

--- single-source_shortest_paths/dijkstra/dijkstra.cpp
+++ single-source_shortest_paths/dijkstra/dijkstra.cpp
@@ -18,11 +18,16 @@ vector<int> pi;
 void dijkstra(int s);
 void initialiseSingleSource(int s);
 void relax(int u, int v, int w);
+void buildMinHeap(vector<int> *A, vector<int> *B);
 void minHeapify(vector<int> *A, vector<int> *B, int i);
 int parent(int i);
 int left(int i);
 int right(int i);
-void troca(vector<int> *a, vector<int> *b);
+void troca(int *a, int *b);
+int heapMinimum(vector<int> B);
+int heapExtractMin(vector<int> *A, vector<int> *B);
+void heapDecreaseKey(vector<int> *A, vector<int> *B, int i, int chave);
+void mostraResultado(void);
 
 int main(int argc, char *argv[]) {
     int n, m;
@@ -45,11 +50,30 @@ int main(int argc, char *argv[]) {
 
     dijkstra(START);
 
+    mostraResultado();
+
     return EXIT_SUCCESS;
 }
 
 void dijkstra(int s) {
+    vector<int> A(adj.size()+1), B(adj.size()+1);
+    int u;
+
     initialiseSingleSource(s);
+    for(uint v=0; v<adj.size(); v++) {
+        A[v+1] = d[v];
+        B[v+1] = v;
+    }
+
+    buildMinHeap(&A, &B);
+
+    while(A.size() > 0) {
+        u = heapExtractMin(&A, &B);
+        for(int i=0; i<adjnum[u]; i++) {
+            relax(u, adj[u][i], pesos[u][i]);
+            heapDecreaseKey(&A, &B, adj[u][i], d[adj[u][i]]);
+        }
+    }
 }
 
 void initialiseSingleSource(int s) {
@@ -61,25 +85,38 @@ void initialiseSingleSource(int s) {
 }
 
 void relax(int u, int v, int w) {
+    if(d[u] == INT_MAX) return;
+
+    if(d[v] > d[u] + w) {
+        d[v] = d[u] + w;
+        pi[v] = u;
+    }
+}
+
+void buildMinHeap(vector<int> *A, vector<int> *B) {
+    for(uint i=((*A).size()-1)/2; i>0; i--) {
+        minHeapify(A, B, i);
+    }
 }
 
 void minHeapify(vector<int> *A, vector<int> *B, int i) {
     uint l=left(i), r=right(i);
     int menor;
 
-    if(l<=(*A).size() && (*A)[l]<(*A)[i]) {
+    if(l<(*A).size() && (*A)[l]<(*A)[i]) {
         menor = l;
     }
     else {
         menor = i;
     }
 
-    if(r<=(*A).size() && (*A)[r]<(*A)[menor]) {
+    if(r<(*A).size() && (*A)[r]<(*A)[menor]) {
         menor = r;
     }
 
     if(menor != i) {
-        troca(&A[i], &A[menor]);
+        troca(&(*A)[i], &(*A)[menor]);
+        troca(&(*B)[i], &(*B)[menor]);
         minHeapify(A, B, menor);
     }
 }
@@ -96,9 +133,62 @@ int right(int i) {
     return 2*i+1;
 }
 
-void troca(vector<int> *a, vector<int> *b) {
+void troca(int *a, int *b) {
+//
+printf("%d %d\n", *a, *b);
+//
     int tmp=*a;
     *a = *b;
     *b = tmp;
 }
 
+int heapMinimum(vector<int> B) {
+    return B[1];
+}
+
+int heapExtractMin(vector<int> *A, vector<int> *B) {
+    int min;
+
+    if((*A).size() < 2) {
+        perror("Erro! Underflow do heap\n");
+        exit(1);
+    }
+
+    min = (*B)[1];
+    (*A)[1] = (*A)[(*A).size()-1];
+    (*B)[1] = (*B)[(*A).size()-1];
+
+    (*A).resize((*A).size()-1);
+    (*B).resize((*B).size()-1);
+
+    minHeapify(A, B, 1);
+
+    return min;
+}
+
+void heapDecreaseKey(vector<int> *A, vector<int> *B, int i, int chave) {
+    if(chave > (*A)[i]) {
+        perror("Erro! Nova chave é maior que a atual\n");
+        exit(1);
+    }
+
+    (*A)[i] = chave;
+
+    while(i>1 && (*A)[parent(i)] > (*A)[i]) {
+        troca(&(*A)[i], &(*A)[parent(i)]);
+        troca(&(*B)[i], &(*B)[parent(i)]);
+        i = parent(i);
+    }
+}
+
+void mostraResultado(void) {
+    printf("Distâncias mínimas do vértice origem (%d)\n", START);
+    for(uint i=0; i<d.size(); i++) {
+        printf("d[%d]: %d\n", i, d[i]);
+    }
+    printf("\nÁrvore de caminhos mínimos\n");
+    for(uint i=0; i<pi.size(); i++) {
+        if(pi[i] == -1) continue;
+        printf("%d->%d\n", pi[i], i);
+    }
+}
